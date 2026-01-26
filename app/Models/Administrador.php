fb894b617f9bc44e375c8c83d42167905f8bb800
cris <cristina.pardosbykova@gmail.com>
2026-01-26 09:17:52 +0100

    Implementación de Modelos vacios.

--- app/Models/Administrador.php
+++ app/Models/Administrador.php
@@ -14,6 +14,6 @@ class Administrador extends Model
     }
 
     public function productOwners() {
-        //return $this->hasMany(ProductOwner::class);
+        return $this->hasMany(ProductOwner::class);
     }
 }
--- app/Models/Comentario.php
+++ app/Models/Comentario.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comentario extends Model
 {
-    //
+    protected $table = 'comentario';
+    protected $fillable = ['texto', 'fecha'];
+
+    public function desarrollador() {
+        return $this->belongsTo(Desarrollador::class);
+    }
+
+    public function productowner() {
+        return $this->belongsTo(ProductOwner::class);
+    }
 }
