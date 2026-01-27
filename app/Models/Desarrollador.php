fcfcbdf6479a8733a5c6598bd02a1ed49a66c016
cris <cristina.pardosbykova@gmail.com>
2026-01-27 10:05:28 +0100

    Quitando roles/permisos

--- app/Models/Desarrollador.php
+++ app/Models/Desarrollador.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Desarrollador extends Model
 {
     protected $table = 'desarrollador';
-    protected $fillable = ['nombre', 'email', 'rol', 'constraseña', 'fecha_alta'];
+    protected $fillable = ['nombre', 'email', 'constraseña', 'fecha_alta'];
 
     public function administrador() {
         return $this->belongsTo(Administrador::class);
