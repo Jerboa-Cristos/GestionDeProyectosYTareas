44cf2417b9139f1b20984e4279b4b5a5f8708fb6
melissa <mervelalv@alu.edu.gva.es>
2026-01-28 13:56:31 +0100

    Proceso de terminar los modelos de product_owner, proyecto, sprint

--- app/Models/ProductOwner.php
+++ app/Models/ProductOwner.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductOwner extends Model
 {
-    //
+    protected $table = 'product_owner';
+
+    protected $fillable = ['nombre', 'email', 'password', 'fecha_alta', 'id_administrador'];
+
+    public function administrador(){
+        return $this->belongsTo(Administrador::class);
+
+    }
+
+    public function proyectos(){
+        return $this->hasMany(Proyecto::class, 'id_product_owner');
+    }
+
+    public function sprints(){
+        return $this->hasManyThrough(Sprint::class, Proyecto::class, 'id_product_owner', 'id_proyecto');
+    }
+
+
 }
--- app/Models/Proyecto.php
+++ app/Models/Proyecto.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Proyecto extends Model
 {
-    //
+    protected $table = 'proyecto';
+
+    protected $fillable = ['nombre', 'descripcion', 'fecha_inicio', 'fecha_fin', 'id_product_owner'];
+
+    public function product_owner(){
+        return $this->belongsTo(ProductOwner::class, 'id_product_owner');
+    }
+
+    public function desarrollador(){
+        return $this->belongsTo(Desarrollador::class, 'id_desarrollador');
+    }
+
+    public function sprints(){
+        return $this->hasMany(Sprint::class, 'id_sprint');
+    }
+
+    public function tareas(){
+        return $this->hasMany(Tarea::class, 'id_tarea');
+    }
+
+
+
 }
