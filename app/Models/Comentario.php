1a328527c262b4a4ee3e748429b05236cab3406c
cris <cristina.pardosbykova@gmail.com>
2026-01-29 13:26:43 +0100

    Correcciones en Modelos y Comentario Controller con las funciones del CRUD

--- app/Models/Comentario.php
+++ app/Models/Comentario.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Comentario extends Model
 {
     protected $table = 'comentario';
-    protected $fillable = ['texto', 'fecha'];
+    protected $fillable = ['texto'];
 
     
     public function desarrollador() {
