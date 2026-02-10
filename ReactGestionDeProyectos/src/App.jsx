e837c9f5dbc28ac7e2a4aaa3e9ec2b6b9e34e9af
Melissa <mervelalv@alu.edu.gva.es>
2026-02-11 00:33:50 +0100

    Añadidas rutas y vista Register

--- ReactGestionDeProyectos/src/App.jsx
+++ ReactGestionDeProyectos/src/App.jsx
@@ -5,6 +5,10 @@ import React from 'react';
 
 import Base from './Components/Base';
 
+//melissa
+import {Routes, Route, Navigate} from 'react-router-dom';
+import Register from './pages/Register/Register';
+
 function rutaApi(){
    return 'http://localhost:8000';
 }
@@ -14,9 +18,36 @@ function App() {
 
 
   return (
-    <div className="bg-blueDark h-screen w-screen">
+
+    {/*
+      <div className="bg-blueDark h-screen w-screen">
       <Base/>
     </div>
+      */},
+
+      <>
+        <Routes>
+          <Route path='/' element={<Navigate to='/register' />}></Route>
+          
+          <Route 
+          path="/base" 
+          element={
+            <>
+              <div className="bg-blueDark h-screen w-screen"></div>
+              <Base />
+            </>
+          }
+          />
+
+          <Route path="/register" element={<Register/>}/> 
+
+        </Routes>
+      
+      
+      </>
+    
+
+    
   )
 }
 
---
+++ ReactGestionDeProyectos/src/pages/Register/Register.jsx
@@ -0,0 +1,12 @@
+function Register () {
+
+
+    return (
+        
+        <>
+            <p>Hello</p>
+        </>
+    )
+}
+
+export default Register
