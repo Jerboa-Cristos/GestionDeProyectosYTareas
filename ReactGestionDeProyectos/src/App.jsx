25a2f95cd5f2f4d252ce63e47f430d018ebcdd78
cris <cristina.pardosbykova@gmail.com>
2026-02-11 13:45:08 +0100

    LoginMoment

--- ReactGestionDeProyectos/src/App.jsx
+++ ReactGestionDeProyectos/src/App.jsx
@@ -2,21 +2,16 @@ import './App.css'
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from './pages/Login/login';
-import Base from './Components/Base';
 
 function App() {
 
   return (
-    <main>
-      <div className="bg-blueDark h-screen w-screen">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Login/>}/>
-          </Routes>
-        </Router>
-      </div>
-    </main>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/DashboardAdmin" element={<dashboardAdmin/>}/>
+        </Routes>
+      </Router>
   )
 }
 
--- ReactGestionDeProyectos/src/pages/Login/login.jsx
+++ ReactGestionDeProyectos/src/pages/Login/login.jsx
@@ -5,10 +5,16 @@ import React, { useEffect } from 'react';
 import Input from '../../Components/Input';
 import Label from '../../Components/Label';
 
+//Se puede hacer una variable const que contenga un string del Tailwind para poder meterlo en el código de página
+const PantallaAzul = "static bg-blueDark h-screen w-screen";
+
+
 function Login() {
-   /* useEffect(()=>{
+    /*useEffect(()=>{
      axios.post(rutaApi()+'/login'), {
         //Aquí ponemos la información que se manda del form
+        login: form.login,
+        password: form.password
 
      }
         .then((response) => {
@@ -21,25 +27,32 @@ function Login() {
     }, []);*/
 
     return(
-        <main>
-            <div className={`bg-white `}>
-                <div>
-                    
-                </div>
-                <h1>Iniciar Sesión</h1>
-                <form onSubmit={useEffect}>
-                    <Input className='Login' placeholder='Login'/>
-                    <Input className='Password' placeholder='Password'/>
-                     <select name='rol'>
+        <div className={PantallaAzul}>
+            <div className={`flex bg-white box-content m-5 w-350 h-195 rounded-md`}>
+                <div className={`grid m-80 items-baseline`}>
+                    <h1>Iniciar Sesión</h1>
+                    <form onSubmit={useEffect}>
+                    <Input 
+                    className='Login' 
+                    type='text' 
+                    placeholder='Login' 
+                    requiered/>
+                    <Input 
+                    className='Password' 
+                    type='password'
+                    placeholder='Password' 
+                    requiered/>
+                    <select name='rol'>
                         <option value="admin">Admin</option>
                         <option value="productOwner">Product Owner</option>
                         <option value="desarrollador">Desarrollador</option>
-                     </select>
-                    <button type='submit'>Iniciar Sesion</button>
-                </form>
+                    </select>
+                    <button type='submit' className={`uppercase`}>Iniciar Sesion</button>
 
+                </form>
+                </div>
             </div>
-        </main>
+        </div>
     )
 }
 
