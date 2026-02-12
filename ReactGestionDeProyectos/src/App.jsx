da35355bb0089282dd8405f6b09ebeb2a1816fe0
Cris <cristina.pardosbykova@gmail.com>
2026-02-12 09:26:26 +0100

    App del master

--- ReactGestionDeProyectos/src/App.jsx
+++ ReactGestionDeProyectos/src/App.jsx
@@ -1,24 +1,53 @@
 import './App.css'
-import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Login from './pages/Login/login';
-import DashboardAdmin from './pages/Administrador/dashboardAdmin';
-import GestionUsuarios from './pages/Administrador/gestionUsuarios';
-import PerfilUsuario from './pages/PerfilUsuario';
-import CreacionUsuarios from './pages/Administrador/CreacionUsuarios';
+
+import axios from 'axios';
+import React, { useEffect } from 'react';
+
+import Base from './Components/Base';
+
+//melissa
+import {Routes, Route, Navigate, useLocation} from 'react-router-dom'
+import Register from './pages/Auth/Register'
+import Login from './pages/Auth/Login'
+import Dashboard from './pages/Auth/Dashboard'
+import Profile from './pages/Auth/Profile'
+import {useState} from 'react'
+
+function rutaApi(){
+   return 'http://localhost:8000';
+}
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const location = useLocation()
+
+  useEffect(() => {
+    const auth = localStorage.getItem("isAuthenticated")
+    setIsAuthenticated(auth === "true")
+  }, [location])
 
   return (
-      <Router>
+    
+
+    
+
+      <>
         <Routes>
-          <Route path="/" element={<Login/>}/>
-          <Route path="/DashboardAdmin" element={<DashboardAdmin/>}/>
-          <Route path="/GestionUsuarios" element={<GestionUsuarios/>}/>
-          <Route path="/PerfilUsuario" element={<PerfilUsuario/>}/>
-          <Route path="/CreacionUsuarios" element={<CreacionUsuarios/>}/>
+          <Route path='/' element={<Navigate to='/register' />} />
+          <Route path="/register" element={<Register/>} />
+          <Route path='/login' element={<Login/>} />
+          {isAuthenticated && (
+            <>
+            <Route path='/dashboard' element={<Dashboard/>} />
+            <Route path="/profile" element={<Profile/>}/>
+            
+            </>
+          )}
+
+          <Route path="/base" element={<><div className="bg-blueDark h-screen w-screen"></div><Base /></>}/>
+          
         </Routes>
-      </Router>
+      </>
   )
 }
 
